3247faf6393fe22871f1d788cc70643b4d502299
Franco Fuentes <franco.fuentes57@gmail.com>
2025-07-08 17:31:33 -0600

    Fix: Background adjustment dark-mode :wrench:

--- src/features/DarkAndLightMode/ThemeToggle.tsx
+++ src/features/DarkAndLightMode/ThemeToggle.tsx
@@ -15,7 +15,7 @@ const ThemeToggle = () => {
     <Button
       variant="outline"
       size="icon"
-      className="rounded-full"
+      className="rounded-full dark:bg-[#0f172a] dark:hover:bg-[#0f172a] transition-all duration-300"
       onClick={handleTheme}
     >
       <FaSun className="absolute h-10 w-10 rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
